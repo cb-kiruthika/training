6616ab51d306a03d2bd2ecff1abe7703e665e794
cb-kiruthika <kiruthika@chargebee.com>
2018-05-18 16:02:18 +0530

    changes in week3/day5

--- week3/day5/DirectoryDB.java
+++ week3/day5/DirectoryDB.java
@@ -20,12 +20,20 @@ class DirectoryDB {
     Connection conn = null;
     Statement stmt = null;
     ResultSet rs = null;
+    PreparedStatement insert = null;
+    PreparedStatement search = null;
+    PreparedStatement update = null;
+    Long searchKey;
+    
     
     void connect() throws SQLException {
         
         //connect to db
         conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" + "user=root&password=");
         stmt = conn.createStatement();
+        insert = conn.prepareStatement("INSERT INTO PD VALUES(NULL,?,?,?,?,?);");
+        search = conn.prepareStatement("SELECT * FROM PD WHERE NAME REGEXP ? OR MOBILE REGEXP ? OR HOME REGEXP ? OR WORK REGEXP ?;");
+        //update = conn.prepareStatement("UPDATE PD SET ? = ? WHERE ID = ?;");
 
     }
 
@@ -35,9 +43,9 @@ class DirectoryDB {
         stmt.executeUpdate("DROP DATABASE IF EXISTS PHONE_DIRECTORY;");
         stmt.executeUpdate("CREATE DATABASE PHONE_DIRECTORY;");
         stmt.executeUpdate("USE PHONE_DIRECTORY;");
-        stmt.executeUpdate("CREATE TABLE PD(NAME VARCHAR(20),ADDRESS VARCHAR(100),MOBILE VARCHAR(10),HOME VARCHAR(10),WORK VARCHAR(10));");
+        stmt.executeUpdate("CREATE TABLE PD(ID BIGINT NOT NULL AUTO_INCREMENT,NAME VARCHAR(20),ADDRESS VARCHAR(100),MOBILE VARCHAR(10),HOME VARCHAR(10),WORK VARCHAR(10),PRIMARY KEY (ID));");
 
-        stmt.executeUpdate("LOAD DATA INFILE \'" + path + " \'INTO TABLE PD FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\\n' IGNORE 1 ROWS;");
+        stmt.executeUpdate("LOAD DATA INFILE \'" + path + " \'INTO TABLE PD FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\\n' IGNORE 1 ROWS (NAME,ADDRESS,MOBILE,HOME,WORK);");
 
     }
 
@@ -56,22 +64,75 @@ class DirectoryDB {
         }
     }
 
-    void searchString(String find) throws SQLException {
+    int searchString(String find) throws SQLException {
         
         //search name and numbers;
-        rs = stmt.executeQuery("SELECT * FROM PD WHERE NAME REGEXP \'" + find + "\' OR MOBILE REGEXP \'" + find + "\' OR HOME REGEXP \'" + find + "\' OR WORK REGEXP \'" + find + "\';");
-        
+        search.setString(1, find);
+        search.setString(2, find);
+        search.setString(3, find);
+        search.setString(4, find);
+        rs = search.executeQuery();
+        int count = 0;
         if (!(rs.next())) {
             System.out.println("NO MATCH");
         } else {
             do {
-                System.out.println("Name: " + rs.getString(1));
-                System.out.println("Address: " + rs.getString(2));
-                System.out.println("Mobile: " + rs.getString(3));
-                System.out.println("Home: " + rs.getString(4));
-                System.out.println("Work: " + rs.getString(5));
+                count++;
+                System.out.println("Name: " + rs.getString(2));
+                System.out.println("Address: " + rs.getString(3));
+                System.out.println("Mobile: " + rs.getString(4));
+                System.out.println("Home: " + rs.getString(5));
+                System.out.println("Work: " + rs.getString(6));
             } while (rs.next());
         }
+        return count;
     }
 
+    void addData(String name, String address, String mobile, String home, String work) throws SQLException {
+        insert.setString(1, name);
+        insert.setString(2, address);
+        insert.setString(3, mobile);
+        insert.setString(4, home);
+        insert.setString(5, work);
+        System.out.println(insert.executeUpdate()+" was inserted");
+    }
+
+
+    boolean updateSearch(String find) throws SQLException {
+        search.setString(1, find);
+        search.setString(2, find);
+        search.setString(3, find);
+        search.setString(4, find);
+        rs = search.executeQuery();
+        int count = 0;
+        if (!(rs.next())) {
+            System.out.println("NO MATCH");
+        } else {
+            do {
+                count++;
+                searchKey = rs.getLong(1);
+                System.out.println("Name: " + rs.getString(2));
+                System.out.println("Address: " + rs.getString(3));
+                System.out.println("Mobile: " + rs.getString(4));
+                System.out.println("Home: " + rs.getString(5));
+                System.out.println("Work: " + rs.getString(6));
+            } while (rs.next());
+        }
+        if(count == 1){
+            return true;
+        }else{
+            return false;
+        }
+    
+    }
+
+    void update(String field, String value) throws SQLException {
+        field = field.toUpperCase();
+        
+        update = conn.prepareStatement("UPDATE PD SET "+field+" = ? WHERE ID = ?;");
+        update.setString(1,value);
+        update.setLong(2, searchKey);
+        System.out.println(update.executeUpdate()+"was updated");
+    }
+    
 }
--- week3/day5/PhoneDirectory.java
+++ week3/day5/PhoneDirectory.java
@@ -7,29 +7,68 @@ import java.util.Scanner;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author cb-kiruthika
  */
 public class PhoneDirectory {
-    public static void main(String[] args) throws SQLException{
-        
+
+    public static void main(String[] args) throws SQLException {
+
         String path = "/Users/cb-kiruthika/training/week3/day5/PhoneDirectory.csv";
-        
+
         DirectoryDB ddb = new DirectoryDB();
         ddb.connect();
         ddb.loadCSV(path);
-        
+
         Scanner in = new Scanner(System.in);
-        String find = in.next();
-        
+        String option = "";
+
         //get search string
-        while (!(find.equals("exit") | find.equals("quit"))) {
-            ddb.searchString(find);
-            find = in.next();
+        while (!(option.equals("exit") | option.equals("quit"))) {
+            System.out.println("search/add/update");
+            option = in.next();
+            if (option.equalsIgnoreCase("search")) {
+                System.out.println("\nEnter string to search");
+                String find = in.next();
+                while (!(find.equals("exit") | find.equals("quit"))) {
+                    ddb.searchString(find);
+                    System.out.println("\nEnter string to search");
+                    find = in.next();
+                }
+            }else if (option.equalsIgnoreCase("add")) {
+                System.out.println("Enter name to add");
+                String name = in.next();
+                System.out.println("Enter address to add");
+                String address = in.next();
+                System.out.println("Enter mobile to add");
+                String mobile = in.next();
+                System.out.println("Enter home number to add");
+                String home = in.next();
+                System.out.println("Enter work number to add");
+                String work = in.next();
+                ddb.addData(name,address,mobile,home,work);
+            }else if (option.equalsIgnoreCase("update")) {
+                
+                System.out.println("\nEnter data to search ");
+                String find = in.next();
+                
+                while(!(ddb.updateSearch(find))){
+                    System.out.println("\nEnter different data to search ");
+                    find = in.next();
+                }
+                
+                System.out.println("Enter name/address/mobile/home/work number to change?");
+                String field = in.next();
+                
+                System.out.println("Enter new value");
+                String value = in.next();
+                ddb.update(field,value);
+                
+            }
+
         }
-        
+
         ddb.disconnect();
     }
 }
