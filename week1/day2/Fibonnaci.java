e5aeb5e7fc10e4604ba45720dbb1d34857df7e99
cb-kiruthika <kiruthika@chargebee.com>
2018-05-28 13:35:48 +0530

    Changes in week1

--- week1/day2/Fibonnaci.java
+++ week1/day2/Fibonnaci.java
@@ -7,22 +7,21 @@ public class Fibonnaci{
                 Scanner in = new Scanner(System.in);
                 System.out.println("Length of series?");
                 System.out.println();
+
                 int n = in.nextInt();
-                if( n == 1 ){
-                    System.out.println("0");
-                }else if(n==2){
-                    System.out.println("0 1");
-                }else{
-                    System.out.println("0 1");
-                    int a = 0;
-                    int b = 1;
+                int a = 0;
+                int b = 1;
+                if(n>=0){
     
-                    for(int i=2;i<n;i++){
+                    for(int i=0;i<n;i++){
                             int c = a + b;
-                            System.out.print(c + " ");
+                            System.out.print(a + " ");
                             a = b;
                             b = c;
                     }        
+                }else{
+                    System.out.println("Enter valid positive length ");
                 }
+                
         }
 }
